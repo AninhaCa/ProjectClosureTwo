23020ec81fae4acfaa2a0df9f25a054405d2d1bc
Aninha <aninhapss2000@gmail.com>
2023-03-23 21:17:23 -0300

    Adde autoLayout

--- ProjectClosureTwo/ProjectClosureTwo/RatingViewController.swift
+++ ProjectClosureTwo/ProjectClosureTwo/RatingViewController.swift
@@ -19,6 +19,10 @@ class RatingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelName.text = "AVALIAÇÃO CLIENTE"
+        labelRating.text = "Para nossos clientes, a pizza \(ratingPizza?.name ?? "") esta na posiçao \(ratingPizza?.rating ?? 0) /5 - de aprovação"
+        let urlImage = URL(string: ratingPizza?.imageURL ?? "")
+        imageRating.sd_setImage(with: urlImage)
     }
 
     @IBAction func back(_ sender: Any) {
